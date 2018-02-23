dc033aef1ea60667bdd679818fa5dae171f305ca
easy <g-easy@users.noreply.github.com>
2018-02-23 16:15:04 +1100

    Start configuring appveyor.

---
+++ tools/appveyor/build.bat
@@ -0,0 +1,19 @@
+REM Copyright 2018, OpenCensus Authors
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+
+REM TODO: Exclude broken proto targets that start with an underscore.
+bazel build ...
+
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
+EXIT /b 0
---
+++ tools/appveyor/install.bat
@@ -0,0 +1,25 @@
+REM Copyright 2018, OpenCensus Authors
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM     http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+
+REM This script is based on:
+REM https://github.com/google/xrtl/blob/master/tools/ci/appveyor/install.bat
+
+IF NOT EXIST %INSTALL_CACHE% (
+  MKDIR %INSTALL_CACHE%
+)
+
+REM Download bazel into install cache, which is on the path.
+IF NOT EXIST %INSTALL_CACHE%\bazel.exe (
+  appveyor DownloadFile https://github.com/bazelbuild/bazel/releases/download/0.10.1/bazel-0.10.1-without-jdk-windows-x86_64.exe -FileName %INSTALL_CACHE%\bazel.exe
+)
