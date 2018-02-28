7106bfc6efc774ac35cf5cba0c27f38d5d1b937c
Ian Sturdy <sturdy@google.com>
2018-02-27 17:14:06 -0800

    Register the gRPC cumulative views in its benchmark.

--- opencensus/plugins/grpc/internal/grpc_plugin_benchmark.cc
+++ opencensus/plugins/grpc/internal/grpc_plugin_benchmark.cc
@@ -95,6 +95,9 @@ BENCHMARK(BM_E2eLatencyCensusDisabled);
 
 void BM_E2eLatencyCensusEnabled(benchmark::State& state) {
   RegisterOnce();
+  // This we can safely repeat, and doing so clears accumulated data to avoid
+  // initialization costs varying between runs.
+  ExperimentalRegisterGrpcViewsForExport();
 
   EchoServerThread server;
   std::unique_ptr<testing::EchoService::Stub> stub =
