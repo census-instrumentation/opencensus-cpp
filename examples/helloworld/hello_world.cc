5143c9eb80eba78fae251758325cf2b6ef829dd7
Vizerai <jsking@google.com>
2018-03-02 18:04:33 -0800

    update

--- examples/helloworld/hello_world.cc
+++ examples/helloworld/hello_world.cc
@@ -27,8 +27,26 @@
 #include "opencensus/trace/sampler.h"
 #include "opencensus/trace/span.h"
 
+ABSL_CONST_INIT const absl::string_view kFrontendKey = "my.org/keys/frontend";
+ABSL_CONST_INIT const absl::string_view kVideoSizeViewName =
+    "my.org/views/video_size";
+ABSL_CONST_INIT const absl::string_view kVideoSizeMeasureName =
+    "my.org/measure/video_size";
+
+opencensus::stats::MeasureInt VideoSizeMeasure();
+
+// The resource owner defines and registers a measure. A function exposing a
+// function-local static is the recommended style, ensuring that the measure is
+// only registered once.
+opencensus::stats::MeasureInt VideoSizeMeasure() {
+  static const opencensus::stats::MeasureInt video_size =
+      opencensus::stats::MeasureRegistry::RegisterInt(
+          kVideoSizeMeasureName, "MBy", "size of processed videos");
+  return video_size;
+}
+
 // Simple program that collects data for video size.
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   if (argc != 1) {
     std::cerr << "Usage: " << argv[0] << "\n";
     return 1;
@@ -39,28 +57,22 @@ int main(int argc, char** argv) {
   opencensus::exporters::stats::StdoutExporter::Register();
   opencensus::exporters::trace::StdoutExporter::Register();
 
-  ABSL_CONST_INIT const absl::string_view FRONTEND_KEY = "my.org/keys/frontend";
-  ABSL_CONST_INIT const absl::string_view VIDEO_SIZE_VIEW_NAME =
-      "my.org/views/video_size";
-  ABSL_CONST_INIT const absl::string_view VIDEO_SIZE_MEASURE_NAME =
-      "my.org/measure/video_size";
-
-  opencensus::stats::MeasureInt VIDEO_SIZE =
-      opencensus::stats::MeasureRegistry::RegisterInt(
-          VIDEO_SIZE_MEASURE_NAME, "MBy", "size of processed videos");
+  // Call measure so that it is initialized.
+  VideoSizeMeasure();
 
   // Create view to see the processed video size distribution broken down by
   // frontend. The view has bucket boundaries (0, 256, 65536) that will group
   // measure values into histogram buckets.
-  const opencensus::stats::ViewDescriptor VIDEO_SIZE_VIEW =
+  const opencensus::stats::ViewDescriptor video_size_view =
       opencensus::stats::ViewDescriptor()
-          .set_name(VIDEO_SIZE_VIEW_NAME)
+          .set_name(kVideoSizeViewName)
           .set_description("processed video size over time")
-          .set_measure(VIDEO_SIZE_MEASURE_NAME)
+          .set_measure(kVideoSizeMeasureName)
           .set_aggregation(opencensus::stats::Aggregation::Distribution(
-              opencensus::stats::BucketBoundaries::Exponential(8, double(1 << 8), 2)))
-          .add_column(FRONTEND_KEY);
-  opencensus::stats::View view(VIDEO_SIZE_VIEW);
+              opencensus::stats::BucketBoundaries::Exponential(8, 1 << 8, 2)))
+          .add_column(kFrontendKey);
+  opencensus::stats::View view(video_size_view);
+  video_size_view.RegisterForExport();
 
   opencensus::trace::AlwaysSampler sampler;
   auto span = opencensus::trace::Span::StartSpan("my.org/ProcessVideo", nullptr,
@@ -71,7 +83,8 @@ int main(int argc, char** argv) {
   span.AddAnnotation("Start processing video.");
   // Sleep for [1,10] milliseconds to fake work.
   absl::SleepFor(absl::Milliseconds(rand() % 10 + 1));
-  opencensus::stats::Record({{VIDEO_SIZE, 25648}}, {{"video1", "video size"}});
+  opencensus::stats::Record({{VideoSizeMeasure(), 25648}},
+                            {{kFrontendKey, "video size"}});
   span.AddAnnotation("Finished processing video.");
   span.End();
 
@@ -80,12 +93,11 @@ int main(int argc, char** argv) {
   absl::SleepFor(absl::Milliseconds(5100));
 
   // Sleep while exporters run in the background.
-  std::cout << "Views:\n" << VIDEO_SIZE_VIEW.DebugString() << "\n";
+  std::cout << "Views:\n" << video_size_view.DebugString() << "\n";
   const auto data = view.GetData();
   if (data.type() == opencensus::stats::ViewData::Type::kDistribution) {
-    for(auto &it : data.distribution_data()) {
-      for(auto &name : it.first)
-        std::cout << name << " : ";
+    for (auto &it : data.distribution_data()) {
+      for (auto &name : it.first) std::cout << name << " : ";
       std::cout << it.second.DebugString() << "\n";
     }
   }
