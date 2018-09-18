fd9ac517b65094bd772ceb238e211d43e39ba7f2
easy <g-easy@users.noreply.github.com>
2018-09-18 16:49:25 -0700

    Simplify stats::StackdriverExporter. (#180)
    This makes it look more like trace::StackdriverExporter, and reduces
    includes.

--- opencensus/exporters/stats/stackdriver/stackdriver_exporter.h
+++ opencensus/exporters/stats/stackdriver/stackdriver_exporter.h
@@ -16,37 +16,26 @@
 #define OPENCENSUS_EXPORTERS_STATS_STACKDRIVER_STACKDRIVER_EXPORTER_H_
 
 #include "absl/strings/string_view.h"
-#include "opencensus/stats/stats.h"
 
 namespace opencensus {
 namespace exporters {
 namespace stats {
 
 // Exports stats for registered views (see opencensus/stats/stats_exporter.h) to
-// Stackdriver.
-// StackdriverExporter is thread-safe.
-class StackdriverExporter : public ::opencensus::stats::StatsExporter::Handler {
+// Stackdriver. StackdriverExporter is thread-safe.
+class StackdriverExporter {
  public:
-  // Registers the exporter and sets the project ID and task value. The
-  // following are required to communicate with stackdriver: a valid project ID,
-  // a valid authentication key which has been generated for that project (with
-  // the "Monitoring Editor" role), and the server security credentials. An
-  // authentication key can be generated for your project in the gcp/stackdriver
-  // settings for your account. Grpc will look for the key using the path
-  // defined by GOOGLE_APPLICATION_CREDENTIALS environment variable (export
-  // GOOGLE_APPLICATION_CREDENTIALS=<path_to_key>). The server security
-  // credentials located in <grpc_path>/etc/roots.pem needs to be copied to
-  // /usr/share/grpc/roots.pem
-  // project_id should be the exact id of the project, as in the GCP console,
-  // with no prefix--e.g. "sample-project-id". opencensus_task is used to
-  // uniquely identify the task in Stackdriver. The recommended format is
+  // Registers the exporter and sets the project ID and task value. project_id
+  // should be the exact id of the project, as in the GCP console, with no
+  // prefix--e.g. "sample-project-id". opencensus_task is used to uniquely
+  // identify the task in Stackdriver. The recommended format is
   // "{LANGUAGE}-{PID}@{HOSTNAME}"; if {PID} is not available a random number
   // may be used.
   static void Register(absl::string_view project_id,
                        absl::string_view opencensus_task);
 
  private:
-  class Handler;
+  StackdriverExporter() = delete;
 };
 
 }  // namespace stats
