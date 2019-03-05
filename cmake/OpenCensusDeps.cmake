86b05133e17f092c117ff5fbd1476eee34823ac2
Mats Taraldsvik <mats.taraldsvik@gmail.com>
2019-03-05 19:29:00 +0100

    file include

--- cmake/OpenCensusDeps.cmake
+++ cmake/OpenCensusDeps.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(FetchContent)
+include(FetchContent.cmake)
 
 fetchcontent_declare(googletest
                      GIT_REPOSITORY
