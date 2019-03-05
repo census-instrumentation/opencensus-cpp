a53c414b855d9c5299e207e917aab0ad2d3bd2cd
Mats Taraldsvik <mats.taraldsvik@gmail.com>
2019-03-05 19:36:51 +0100

    fix

--- cmake/OpenCensusDeps.cmake
+++ cmake/OpenCensusDeps.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(FetchContent.cmake)
+include(FetchContent)
 
 fetchcontent_declare(googletest
                      GIT_REPOSITORY
