43b62e0807bed8aefca912d5df50275da2b44a05
easy <g-easy@users.noreply.github.com>
2019-03-14 14:25:40 +1100

    gRPC calls moved to grpc_impl namespace.
    This fixes the build.
    
    See also: https://github.com/grpc/grpc/pull/18223

--- examples/grpc/hello_client.cc
+++ examples/grpc/hello_client.cc
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
   }
 
   // Register the OpenCensus gRPC plugin to enable stats and tracing in gRPC.
-  grpc::RegisterOpenCensusPlugin();
+  grpc_impl::RegisterOpenCensusPlugin();
 
   // Register exporters for Stackdriver.
   RegisterStackdriverExporters();
