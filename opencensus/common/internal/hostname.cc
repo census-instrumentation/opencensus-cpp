b130c8133a499883eb55ddb20b4295352acd2989
easy <g-easy@users.noreply.github.com>
2019-07-25 15:19:05 +1000

    WSAStartup/Cleanup around gethostname.

--- opencensus/common/internal/hostname.cc
+++ opencensus/common/internal/hostname.cc
@@ -15,8 +15,11 @@
 #include "opencensus/common/internal/hostname.h"
 
 #if defined(_MSC_VER)
+#define WIN32_LEAN_AND_MEAN
 #include <process.h>
-#include <winsock.h>
+#include <windows.h>
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
 #else
 #include <unistd.h>
 #endif
@@ -25,7 +28,27 @@
 
 namespace opencensus {
 namespace common {
+namespace {
+constexpr char kUnknownHostname[] = "unknown_hostname";
+}  // namespace
 
+#if defined(_MSC_VER)
+std::string Hostname() {
+  WSADATA data;
+  if (WSAStartup(MAKEWORD(2, 2), &data) != 0) {
+    return kUnknownHostname;
+  }
+  // SUSv2 says 255 is the limit for hostnames.
+  char buf[256];
+  if (gethostname(buf, sizeof(buf)) != 0) {
+    return kUnknownHostname;
+  }
+  // NUL terminate just in case.
+  buf[sizeof(buf) - 1] = 0;
+  WSACleanup();
+  return buf;
+}
+#else
 std::string Hostname() {
   // SUSv2 says 255 is the limit for hostnames.
   char buf[256];
@@ -36,6 +59,7 @@ std::string Hostname() {
   buf[sizeof(buf) - 1] = 0;
   return buf;
 }
+#endif
 
 std::string OpenCensusTask() {
   return absl::StrCat("cpp-", getpid(), "@", Hostname());
