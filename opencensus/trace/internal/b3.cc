3774196d718d5c933bb01eff208339e90de46190
easy <g-easy@users.noreply.github.com>
2019-07-26 13:22:16 +1000

    Revert "Optimize 64-bit trace_id."
    This reverts commit 6dcda4d354f3463f48797337424d5a31fae76b10.
    
    It made things 2ns slower.
    
    Before:
    BM_FromB3Headers_64bitTraceId         68.4 ns         68.4 ns    101944542
    
    After "optimize":
    BM_FromB3Headers_64bitTraceId         70.6 ns         70.6 ns     99264778

--- opencensus/trace/internal/b3.cc
+++ opencensus/trace/internal/b3.cc
@@ -19,7 +19,6 @@
 
 #include "absl/strings/ascii.h"
 #include "absl/strings/escaping.h"
-#include "absl/strings/str_cat.h"
 #include "opencensus/trace/span_context.h"
 #include "opencensus/trace/span_id.h"
 #include "opencensus/trace/trace_id.h"
@@ -77,9 +76,7 @@ SpanContext FromB3Headers(absl::string_view b3_trace_id,
 
   // Extend 64-bit trace_id to 128-bit.
   if (b3_trace_id.length() == 16) {
-    constexpr char eight_nuls_str[] = "\0\0\0\0\0\0\0\0";
-    ABSL_CONST_INIT const absl::string_view eight_nuls(eight_nuls_str, 8);
-    trace_id_binary = absl::StrCat(eight_nuls, trace_id_binary);
+    trace_id_binary = std::string(8, '\0') + trace_id_binary;
   }
 
   return SpanContext(
